b45eb4604319ea9438d9124e7977c39ce34e2672
pjozwik3 <przemyslaw.jozwik3@pw.edu.pl>
2023-02-24 03:06:16 +0100

    Function tweaks. Now it works.

--- IPv4_prefix_manager/IPv4_prefix_manager.c
+++ IPv4_prefix_manager/IPv4_prefix_manager.c
@@ -5,14 +5,15 @@
 #include "IPv4_prefix_manager.h"
 
 
-int del(struct PrefixDataBase *dataBase, unsigned int base, char mask) {
-    //Mask length must be unsigned integer from range 0 to 32 and base must be unsigned integer
-    //from 0 to INT_MAX (max base number possible)
-    if(mask > (char)32||mask < 0||dataBase->size == 0) return -1;
+int del(struct PrefixDatabase *dataBase, unsigned int base, char mask, const char *status) {
+    if(dataBase->size == 0 || *status == -1) {
+        return -1;
+    }
 
     for(int i = 0; i<dataBase->size; i++) {
-        if(dataBase->data[i].base==base && dataBase->data[i].mask == mask) {
-            for(int j = i; j< dataBase->size -1 ; j++) { //Moving
+        if(dataBase->data[i].base==base && dataBase->data[i].maskLength == mask) {
+            //Shifting data
+            for(int j = i; j < dataBase->size-1 ; j++) {
                 dataBase->data[j] = dataBase->data[j+1];
             }
             dataBase->size--;
@@ -22,99 +23,116 @@ int del(struct PrefixDataBase *dataBase, unsigned int base, char mask) {
     //No prefix found
     return -1;
 }
-int add(struct PrefixDataBase *dataBase, unsigned int base, char mask) {
-
-    if(mask > (char)32 || mask < 0 || dataBase->size >= MAX_DATA_SIZE)
+int add(struct PrefixDatabase *dataBase, unsigned int base, char mask, char *status) {
+    if(dataBase->size >= MAX_DATA_SIZE || *status == -1) {
         return -1;
-
-    unsigned tempIndex = 0;
+    }
+    unsigned insertIndex = 0;
+    struct Prefix prefixBuff;
+    prefixBuff.maskLength = mask;
+    prefixBuff.subnetMask = maskLengthToSubnetConv(mask, status);
+    //Checking if mask is proper
+    if (*status == -1) {
+        return -1;
+    }
+    //Calculating network address based on base and mask
+    prefixBuff.base = base&prefixBuff.subnetMask;
 
     if(dataBase->size == 0) {
-        tempIndex = 0;
+        insertIndex = 0;
     } else {
         for(int i = 0; i < dataBase->size; i++) {
-            if(mask > dataBase->data[i].mask){
-                tempIndex = i;
-                //Setting temporary index to iteration in which smaller mask was found
+            if(prefixBuff.maskLength > dataBase->data[i].maskLength){
+                insertIndex = i;
+                // Setting temporary index to iteration in which smaller maskLength was found
                 break;
-            } else if(dataBase->data[i].base == base &&
-                dataBase->data[i].mask == mask) {
+            } else if(dataBase->data[i].base == prefixBuff.base &&
+                dataBase->data[i].maskLength == prefixBuff.maskLength) {
                 return -1;
-                //This Prefix is already in the database
+                // This Prefix is already in the database
             } else if(i == dataBase->size - 1) {
-                //No prefix mask length in the database is less than new prefix mask length case
-                tempIndex = dataBase->size;
+                // Case in witch no prefix maskLength length in the database is less than new prefix
+                // maskLength
+                insertIndex = dataBase->size;
             }
         }
     }
     //Shifting data
-    for(int j = (int)tempIndex; j > dataBase->size; j++) {
-        dataBase->data[j+1] = dataBase->data[j];
+
+    for(int j = (int)dataBase->size; j > insertIndex; j--) {
+        dataBase->data[j] = dataBase->data[j-1];
     }
-
-    dataBase->data[tempIndex].base = base;
-    dataBase->data[tempIndex].mask = mask;
-    dataBase->data[tempIndex].subnetMask = maskLengthToSubnetConv(mask);
-
     dataBase->size++;
+    dataBase->data[insertIndex]=prefixBuff;
 
     return 0;
 }
 
-char check(struct PrefixDataBase *dataBase, unsigned int ip) {
-
-    for(int i = (int)dataBase->size; i>=0; i--) {
-        if((ip & dataBase->data[dataBase->size - i].subnetMask) ==
-            dataBase->data[dataBase->size - i].base) {
-            return dataBase->data[dataBase->size - i].mask;
-            //Length of the mask is the longest possible, because database is sorted by mask length
+char check(struct PrefixDatabase *dataBase, unsigned int ip, const char *status) {
+    if (*status == -1) {
+        return -1;
+    }
+    for(int i = 0; i < dataBase->size; i++) {
+        if((ip & dataBase->data[i].subnetMask) ==
+            dataBase->data[i].base) {
+            return dataBase->data[i].maskLength;
+            // Length of the maskLength is the longest possible, because database is sorted by
+            // maskLength
         }
     }
+    // Not found ip in Prefix database
     return -1;
-    //Not found ip in Prefix database
+
 }
 
-unsigned int IPv4_stringToIntConv(const char *ip) {
-
+unsigned int IPv4_stringToIntConv(const char *ip, char *status) {
     const char *start = ip;
     char *end;
-    unsigned int a = strtoul(start, &end, 10);
-    if (*end != '.' || a > 255) {
-        printf("\nIP address is wrong\n");
-        return 0;
-    }
-    unsigned int b = strtoul(end + 1, &end, 10);
-    if (*end != '.' || b > 255) {
-        printf("\nIP address is wrong\n");
-        return 0;
-    }
-    unsigned int c = strtoul(end + 1, &end, 10);
-    if (*end != '.'|| c > 255) {
-        printf("\nIP address is wrong\n");
-        return 0;
-    }
-    unsigned int d = strtoul(end + 1, &end, 10);
-    if (*end || d > 255) {
-        printf("\nIP address is wrong\n");
-        return 0;
-    }
+    unsigned int result = 0;
+    unsigned int temp;
 
-    return ((a << 24) | (b << 16) | (c << 8) | d);
+    temp = strtoul(start, &end, 10);
+    if (*end != '.' || temp > 255) {
+        printf("\nIP address is wrong\n");
+        *status = -1;
+        return 0;
+    }else result |= temp << 24;
+
+    temp = strtoul(end + 1, &end, 10);
+    if (*end != '.' || temp > 255) {
+        printf("\nIP address is wrong\n");
+        *status = -1;
+        return 0;
+    }else result |= temp << 16;
+
+    temp = strtoul(end + 1, &end, 10);
+    if (*end != '.'|| temp > 255) {
+        printf("\nIP address is wrong\n");
+        *status = -1;
+        return 0;
+    }else result |= temp << 8;
+
+    temp = strtoul(end + 1, &end, 10);
+    if (*end || temp > 255) {
+        printf("\nIP address is wrong\n");
+        *status = -1;
+        return 0;
+    }else result |= temp;
+    *status = 0;
+    return result;
 }
 
-unsigned int maskLengthToSubnetConv(char mask) {
-    unsigned int mask_buff = 0;
-    for(unsigned int i = (unsigned int)mask; i > 0; i--) {
-        mask_buff += binPow(32-i);
+unsigned int maskLengthToSubnetConv(char maskLength, char *status) {
+    if(maskLength < 0 || maskLength > 32) {
+        printf("\nMask length is wrong\n");
+        *status = -1;
+        return 0;
     }
+    if(maskLength == 32) return UINT_MAX;
+    unsigned int mask_buff = (1 << (unsigned)maskLength)-1;
+    mask_buff = mask_buff << (32 - maskLength);
+
+    *status = 0;
     return mask_buff;
 }
 
-unsigned int binPow(unsigned int power) {
-    if(power == 0) return 0;
-    unsigned result = 2;
-    for(; power>1; power--) {
-        result *= 2;
-    }
-    return result;
-}
--- IPv4_prefix_manager/IPv4_prefix_manager.h
+++ IPv4_prefix_manager/IPv4_prefix_manager.h
@@ -18,25 +18,25 @@ struct __attribute__((__packed__))Prefix {
     unsigned int base;
     unsigned int subnetMask;
     //Conversion from subnetMask length into subnetMask
-    char mask;
+    char maskLength;
 };
 
-//Struct which contains sorted prefixes from the highest mask length number to its lowest number.
-struct __attribute__((__packed__))PrefixDataBase {
+//Struct which contains sorted prefixes from the highest maskLength length number to its lowest
+//number
+struct __attribute__((__packed__)) PrefixDatabase {
     struct Prefix data[MAX_DATA_SIZE];
     unsigned int size;
 };
 //Function definitions
-int add(struct PrefixDataBase *dataBase, unsigned int base, char mask);
-int del(struct PrefixDataBase *dataBase, unsigned int base, char mask);
-inline char check(struct PrefixDataBase *dataBase, unsigned int ip);
+int add(struct PrefixDatabase *dataBase, unsigned int base, char mask, char *status);
+int del(struct PrefixDatabase *dataBase, unsigned int base, char mask, const char *status);
+inline char check(struct PrefixDatabase *dataBase, unsigned int ip, const char *status);
 
 //Increases the number 2 to the specified power. Is used by maskLengthToSubnetConv function
-unsigned int binPow(unsigned int);
-//Changes string ip address to unsigned.
-unsigned int IPv4_stringToIntConv(const char *ip);
+
+unsigned int IPv4_stringToIntConv(const char *ip, char *status);
 //Changes subnetMask length into decimal number of subnetMask.
-unsigned int maskLengthToSubnetConv(char mask);
+unsigned int maskLengthToSubnetConv(char maskLength, char *status);
 
 
 #endif//PHOENIXSYSTEMS_IPV4_PREFIX_IPV4_PREFIX_MANAGER_H
